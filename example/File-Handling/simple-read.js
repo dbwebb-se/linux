add2ca6780761655d095e6e36ec67b7da9b56b1d
Jonatan Karlsson <me@jonatankarlsson.se>
2015-06-15 10:06:02 +0200

    added simple read file/dir example

---
+++ example/File-Handling/simple-read.js
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+ 
+var fs = require('fs');
+
+// Reads the current directory
+var files = fs.readdirSync(__dirname);
+
+// Loops through the array of filename and prints them
+for (var i = 0; i < files.length; i++) {
+	console.log(files[i]);
+}
+
+/**
+ *  Read a file and print the content in the console
+ */
+// Reads an utf-8 encoded file
+var fileContent = fs.readFileSync('./data.txt', 'utf8');
+
+// Print the content to console
+console.log(fileContent);
+
+
