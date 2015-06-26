e1c5f147d350385379a768a184af489e8639e480
Mikael Roos <mikael.t.h.roos@gmail.com>
2015-06-26 15:06:14 +0200

    new structure for example programs in sub directories

--- example/simpleServer/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * Main program to run a simple server that says Hello World.
- *
- */
-import server from './server.js';
-
-// Start the server to listen on a port
-server.listen(1337);
-console.log("Simple server listen on port 1337.");
