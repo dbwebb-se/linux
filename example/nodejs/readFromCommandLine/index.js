0cac48c7bc3d076379e7017adcaed7267b68a7bb
Jonatan Karlsson <me@jonatankarlsson.se>
2015-06-30 11:16:26 +0200

    Added example on how to read from command line in nodejs

---
+++ example/nodejs/readFromCommandLine/index.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env nodejs
+
+/**
+ * Execute this project by:
+ * $ babel-node index.js
+ */
+
+var usage = 'Read passed parameters to the script.\n ';
+
+console.log(usage);
+
+console.log('All arguments passed. Note. that "node" and ' +
+            'the "file" is also included.');
+
+console.log('All arguments as an array: \n', process.argv);
+
+// Save the passed arguments
+var argumentsArray = process.argv;
+
+// You can index on the arguments just like any other array
+var last = argumentsArray[argumentsArray.length - 1];
+
+console.log('Last passed argument: ', last);
