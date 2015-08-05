68ae5b7928f0363ce2ce867b0d413c8ad95d9435
Jonatan Karlsson <me@jonatankarlsson.se>
2015-08-05 10:54:23 +0200

    :shirt: clean up

--- example/nodejs/router/usage.js
+++ example/nodejs/router/usage.js
@@ -6,46 +6,7 @@ import Router from '../router/router';
 var http = require('http');
 var router = new Router();
 
-/*function hello(req, res) {
-    res.end('Hello');
-}
-
-// Standard 'GET /' using the function hello.
-router.add('GET', '/', hello);
-
-// Standard 'GET /hello' using the function hello.
-router.add('GET', '/hello', hello);
-
-// 'GET /foo' with anonymous function.
-router.add('GET', '/foo', function (req, res) {
-    res.end('FOO');
-});
-
-// 'GET /helloworld' with anonymous function.
-router.add('GET', '/helloworld', function (req, res) {
-    res.end('Hello WORLD');
-});
-
-// 'GET /blabla' using ES6 arrow function.
-router.get('/blabla', (req, res) => {
-    res.end('blabla');
-});
-
-// 'POST /hello' with anonymous function.
-router.add('POST', '/hello', function (req, res) {
-    res.end('POST REQUEST');
-});
-*/
-
-/*router.get('/animal', (req, res) => {
-    res.end('Animal');
-});
-*/
-
 router.get('/animal/', (req, res) => {
-    /*console.log(req);
-    console.log('-----------------');
-    console.log(res);*/
     res.end('Animal with a slash');
 });
 
