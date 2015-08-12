77cc09d132e4b5b240f566525fee19289681ff22
Mikael Roos <mikael.t.h.roos@gmail.com>
2015-08-12 10:41:40 +0200

    require camelcase filename

--- example/nodejs/serverUptime/index.js
+++ example/nodejs/serverUptime/index.js
@@ -2,8 +2,9 @@
  * Main program to run a simple server that says Hello World.
  *
  */
-import server from './server-uptime.js';
+import server from './serverUptime.js';
 
 // Start the server to listen on a port
 server.listen(1337);
+
 console.log("Simple server replying with output of command uptime, listen on port 1337.");
