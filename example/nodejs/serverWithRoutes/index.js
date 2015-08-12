34a758b6804919d7ca6d6d4d4756b82a1faadc1e
Mikael Roos <mikael.t.h.roos@gmail.com>
2015-08-12 12:00:55 +0200

    updating serverwithroutes

--- example/nodejs/serverWithRoutes/index.js
+++ example/nodejs/serverWithRoutes/index.js
@@ -2,8 +2,10 @@
  * Main program to run a simple server that says Hello World.
  *
  */
-import server from './server-with-routes.js';
+import server from './serverWithRoutes.js';
 
 // Start the server to listen on a port
 server.listen(1337);
-console.log('Simple server with routes listen on port 1337. Try / and /about and /whatever');
+
+console.log('Simple server with routes listen on port 1337.');
+console.log('Try routes / and /about and /whatever');
