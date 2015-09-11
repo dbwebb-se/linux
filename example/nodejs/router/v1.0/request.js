ed412cb0ebbb511d0c68af50ab404f3c54b09b38
Olund <henke.olund@gmail.com>
2015-09-11 13:59:33 +0200

    Moved old files to another folder

---
+++ example/nodejs/router/v1.0/request.js
@@ -0,0 +1,25 @@
+/**
+ * Response
+ */
+
+var url = require('url');
+
+/**
+ * @version 1.0
+ */
+export function buildRequest (req, res) {
+    // stupid jshint gives error on res not being used..
+    /*jshint unused:false*/
+    req = req || {};
+    req.params = {};
+    req.query = url.parse(req.url, true).query;
+    req.queryParts = url.parse(req.url, true);
+
+    req.rawBody  = '';
+    // Takes care of any data that comes frome a post-request
+    req.on('data', (chunk) => {
+        req.rawBody += chunk;
+    });
+
+    return req;
+}
